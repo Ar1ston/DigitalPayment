ec4899b5b10504b08441ff45216610efac70ceb2
Вадим Злобин <v.zlobin@sendy.land>
2022-07-20 15:08:19 +0300

    update

---
+++ Services/Users/Workers/Listener.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"DigitalPayment/Services/Authors/Requests"
+	"DigitalPayment/lib/NATS"
+	"fmt"
+)
+
+func main() {
+
+	conn := NATS.ConnectNATS{
+		Host: "localhost",
+		Port: "4222",
+	}
+	nats, err := conn.ConnectToNATS()
+	if err != nil {
+		fmt.Printf("Ошибка подключения к NATS: %s", err)
+		return
+	}
+	Requests.Hello()
+	NATS.RunWorker(nats, "Users")
+}
