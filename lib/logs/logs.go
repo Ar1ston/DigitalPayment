0e1025bb3b6147ab9859d6facd2932cad5b954a5
Вадим Злобин <v.zlobin@sendy.land>
2022-08-31 11:48:25 +0300

    fix logs

--- lib/logs/logs.go
+++ lib/logs/logs.go
@@ -29,16 +29,16 @@ func (log *Log) SetRequest(requestName string) *Log {
 }
 func (log *Log) Printf(format string, params ...interface{}) {
 	if log.requestName != "" {
-		fmt.Printf("[%s][%s] %s\n", log.serviceName, log.requestName, fmt.Sprintf(format, params))
+		fmt.Printf("[%s][%s] %s\n", log.serviceName, log.requestName, fmt.Sprintf(format, params...))
 	} else {
-		fmt.Printf("[%s] %s\n", log.serviceName, fmt.Sprintf(format, params))
+		fmt.Printf("[%s] %s\n", log.serviceName, fmt.Sprintf(format, params...))
 	}
 }
 func (log *Log) Errorf(format string, params ...interface{}) {
 	if log.requestName != "" {
-		fmt.Printf("[%s][%s][%s] %s\n", log_error, log.serviceName, log.requestName, fmt.Sprintf(format, params))
+		fmt.Printf("[%s][%s][%s] %s\n", log_error, log.serviceName, log.requestName, fmt.Sprintf(format, params...))
 	} else {
-		fmt.Printf("[%s][%s] %s\n", log_error, log.serviceName, fmt.Sprintf(format, params))
+		fmt.Printf("[%s][%s] %s\n", log_error, log.serviceName, fmt.Sprintf(format, params...))
 	}
 }
 func (log *Log) Error(str string) {
@@ -50,9 +50,9 @@ func (log *Log) Error(str string) {
 }
 func (log *Log) Infof(format string, params ...interface{}) {
 	if log.requestName != "" {
-		fmt.Printf("[%s][%s][%s] %s\n", log_info, log.serviceName, log.requestName, fmt.Sprintf(format, params))
+		fmt.Printf("[%s][%s][%s] %s\n", log_info, log.serviceName, log.requestName, fmt.Sprintf(format, params...))
 	} else {
-		fmt.Printf("[%s][%s] %s\n", log_info, log.serviceName, fmt.Sprintf(format, params))
+		fmt.Printf("[%s][%s] %s\n", log_info, log.serviceName, fmt.Sprintf(format, params...))
 	}
 }
 func (log *Log) Info(str string) {
@@ -64,9 +64,9 @@ func (log *Log) Info(str string) {
 }
 func (log *Log) Debugf(format string, params ...interface{}) {
 	if log.requestName != "" {
-		fmt.Printf("[%s][%s][%s] %s\n", log_debug, log.serviceName, log.requestName, fmt.Sprintf(format, params))
+		fmt.Printf("[%s][%s][%s] %s\n", log_debug, log.serviceName, log.requestName, fmt.Sprintf(format, params...))
 	} else {
-		fmt.Printf("[%s][%s] %s\n", log_debug, log.serviceName, fmt.Sprintf(format, params))
+		fmt.Printf("[%s][%s] %s\n", log_debug, log.serviceName, fmt.Sprintf(format, params...))
 	}
 }
 func (log *Log) Debug(str string) {
