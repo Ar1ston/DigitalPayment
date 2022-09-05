e8fbb7fe9acce77c0b7504ec430722ee58e26ece
Ar1ston <zlobinvadim@gmail.com>
2022-09-05 20:44:10 +0300

    update SQL scripts

--- pg_dump/authors.sql
+++ pg_dump/authors.sql
@@ -1,100 +1,11 @@
---
--- PostgreSQL database dump
---
 
--- Dumped from database version 14.2
--- Dumped by pg_dump version 14.2
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
---
--- Name: authors; Type: TABLE; Schema: public; Owner: postgres
---
-
-CREATE TABLE public.authors (
-    id integer NOT NULL,
-    "firstName" character varying DEFAULT ''::character varying NOT NULL,
-    "lastName" character varying DEFAULT ''::character varying NOT NULL,
-    "description" character varying DEFAULT ''::character varying NOT NULL
+CREATE TABLE public.authors(
+    id serial PRIMARY KEY,
+    firstName CHARACTER VARYING(30) DEFAULT '' NOT NULL ,
+    lastName  CHARACTER VARYING(30) DEFAULT '' NOT NULL ,
+    description TEXT
 );
 
-
-ALTER TABLE public.authors OWNER TO postgres;
-
---
--- Name: authors_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.authors_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.authors_id_seq OWNER TO postgres;
-
---
--- Name: authors_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.authors_id_seq OWNED BY public.authors.id;
-
-
---
--- Name: authors id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.authors ALTER COLUMN id SET DEFAULT nextval('public.authors_id_seq'::regclass);
-
-
---
--- Data for Name: authors; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.authors (id, "firstName", "lastName", "description") FROM stdin;
-1	Неизвестно	Неизвестно	Неизвестно
-\.
-
-
---
--- Name: authors_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.authors_id_seq', 1, true);
-
-
---
--- Name: authors authors_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.authors
-    ADD CONSTRAINT authors_pk PRIMARY KEY (id);
-
-
---
--- Name: authors_id_uindex; Type: INDEX; Schema: public; Owner: postgres
---
-
-CREATE UNIQUE INDEX authors_id_uindex ON public.authors USING btree (id);
-
-
---
--- PostgreSQL database dump complete
---
-
+INSERT INTO public.authors (firstName, lastName, description) VALUES ('Александр','Блок','Известный русский писатель эпохи просвещения');
+INSERT INTO public.authors (firstName, lastName, description) VALUES ('Александр','Пушкин','Русский поэт, драматург и прозаик, заложивший основы русского реалистического направления ');
+INSERT INTO public.authors (firstName, lastName, description) VALUES ('Лев','Толстой','один из наиболее известных русских писателей и мыслителей, один из величайших писателей-романистов мира');
--- pg_dump/books.sql
+++ pg_dump/books.sql
@@ -1,103 +1,11 @@
---
--- PostgreSQL database dump
---
-
--- Dumped from database version 14.2
--- Dumped by pg_dump version 14.2
-
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-SET default_tablespace = '';
-
-SET default_table_access_method = heap;
-
---
--- Name: books; Type: TABLE; Schema: public; Owner: postgres
---
 
 CREATE TABLE public.books (
-    id integer NOT NULL,
-    author integer DEFAULT 1 NOT NULL,
-    publisher integer DEFAULT 1 NOT NULL,
-    "addedUser" integer DEFAULT 1 NOT NULL,
+    id SERIAL PRIMARY KEY ,
+    author BIGINT DEFAULT 1 NOT NULL CHECK ( author>0 ),
+    publisher BIGINT DEFAULT 1 NOT NULL CHECK ( publisher>0 ),
+    "addedUser" BIGINT DEFAULT 1 NOT NULL CHECK ("addedUser">0),
     "addedTime" timestamp without time zone,
-    "description" character varying DEFAULT 'Неизвестно'::character varying NOT NULL,
-    "name" character varying DEFAULT 'Неизвестно'::character varying NOT NULL,
-    "genre" character varying DEFAULT 'Неизвестно'::character varying NOT NULL
+    "description" TEXT DEFAULT 'Нет описания' NOT NULL,
+    "name" TEXT NOT NULL CHECK ( "name"!='' ),
+    "genre" TEXT NOT NULL CHECK ( genre!='' )
 );
-
-
-ALTER TABLE public.books OWNER TO postgres;
-
---
--- Name: books_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
---
-
-CREATE SEQUENCE public.books_id_seq
-    AS integer
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
-ALTER TABLE public.books_id_seq OWNER TO postgres;
-
---
--- Name: books_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
---
-
-ALTER SEQUENCE public.books_id_seq OWNED BY public.books.id;
-
-
---
--- Name: books id; Type: DEFAULT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.books ALTER COLUMN id SET DEFAULT nextval('public.books_id_seq'::regclass);
-
-
---
--- Data for Name: books; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-COPY public.books (id, author, publisher, "addedUser", "addedTime", "description", "name", "genre") FROM stdin;
-\.
-
-
---
--- Name: books_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.books_id_seq', 1, false);
-
-
---
--- Name: books books_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
---
-
-ALTER TABLE ONLY public.books
-    ADD CONSTRAINT books_pk PRIMARY KEY (id);
-
-
---
--- Name: books_id_uindex; Type: INDEX; Schema: public; Owner: postgres
---
-
-CREATE UNIQUE INDEX books_id_uindex ON public.books USING btree (id);
-
-
---
--- PostgreSQL database dump complete
---
-
